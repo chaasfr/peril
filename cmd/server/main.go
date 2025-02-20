6c4620a01f6f3efc2c2700e2dd041d3700586a96
chaasfr <chaasfr@gmail.com>
2025-02-20 22:43:52 +0100

    starting REPL on server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
+	// "os"
+	// "os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+
+
 func main() {
 	log.Println("Starting Peril server...")
 	connectionString := "amqp://guest:guest@localhost:5672/"
@@ -24,19 +28,50 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	pubsub.PublishJSON(
-		amqpChan,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{IsPaused: true},
-	)
 
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan 
-	log.Println("shutting down")
-	connection.Close()
+	gamelogic.PrintServerHelp()
+	stop := false
+	for {
+		if stop {
+			break
+		}
+
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch(words[0]) {
+		case "pause":
+			log.Println("sending a pause message")
+			pubsub.PublishJSON(
+				amqpChan,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: true},
+			)
+		case "resume":
+			log.Println("sending a resume message")
+			pubsub.PublishJSON(
+				amqpChan,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: false},
+			)
+		case "quit":
+			log.Println("exiting")
+			stop = true
+		default:
+			log.Println("I donùt understand this command")
+		}
+	}
+
+
+	// // wait for ctrl+c
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan 
+	// log.Println("shutting down")
+	// connection.Close()
 	
 }
