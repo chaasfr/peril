bad4fb72054ccd4bd36ac85c0c2e25d22fcd545f
chaasfr <chaasfr@gmail.com>
2025-02-23 12:20:52 +0100

    added spam functionnality

--- cmd/client/repl.go
+++ cmd/client/repl.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -24,12 +26,33 @@ func repl(gs *gamelogic.GameState, amqpChan *amqp.Channel) {
 		}
 		switch(words[0]) {
 		case "spawn":
-			err := gs.CommandSpawn(words)
-			if err != nil {
-				log.Println(err)
-			}
+			replSpawn(gs, words)
 		case "move":
-			move, err := gs.CommandMove(words)
+			replMove(gs, amqpChan, words)
+		case "status":
+			gs.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			replSpam(gs, amqpChan, words)
+		case "quit":
+			gamelogic.PrintQuit()
+			stop = true
+		default:
+			log.Println("unknown command.")
+		}
+	}
+}
+
+func replSpawn(gs *gamelogic.GameState, words []string) {
+	err := gs.CommandSpawn(words)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func  replMove(gs *gamelogic.GameState, amqpChan *amqp.Channel, words []string) {
+	move, err := gs.CommandMove(words)
 			if err == nil {
 				log.Println("move successful")
 				err = pubsub.PublishJSON(amqpChan, routing.ExchangePerilTopic, fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, gs.Player.Username), move)
@@ -38,17 +61,30 @@ func repl(gs *gamelogic.GameState, amqpChan *amqp.Channel) {
 			if err != nil {
 				log.Println(err)
 			}
-		case "status":
-			gs.CommandStatus()
-		case "help":
-			gamelogic.PrintClientHelp()
-		case "spam":
-			log.Println("Spamming not allowed yet!")
-		case "quit":
-			gamelogic.PrintQuit()
-			stop = true
-		default:
-			log.Println("unknown command.")
-		}
+}
+
+func replSpam(gs *gamelogic.GameState, amqpChan *amqp.Channel, words []string) {
+	if len(words) < 2 {
+		log.Println("usage: spam <int>")
+		return
+	}
+	n, err := strconv.Atoi(words[1])
+	if err != nil {
+		log.Println("please use an integer instead of "+ words[1])
+		return
+	}
+
+	for range n {
+		maliciousLog := gamelogic.GetMaliciousLog()
+		pubsub.PublishGob(
+			amqpChan,
+			routing.ExchangePerilTopic,
+			fmt.Sprintf("%s.%s", routing.GameLogSlug,gs.Player.Username),
+			routing.GameLog{
+				CurrentTime: time.Now(),
+				Message: maliciousLog,
+				Username: gs.Player.Username,
+			},
+		)
 	}
 }
